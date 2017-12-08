f5bc0cbee26e4c731038b1b95f0cc65943eecae4
LTT <578927974@qq.com>
2017-12-08 09:42:37 +0800

    nunit test

--- NNCMS/UnitTestNNCMS/AuthenticationTest.cs
+++ NNCMS/UnitTestNNCMS/AuthenticationTest.cs
@@ -1,6 +1,7 @@
 using System;
 using NUnit.Framework;
 using IdentityModel.Client;
+using System.Net.Http;
 
 namespace UnitTestNNCMS
 {
@@ -8,12 +9,31 @@ namespace UnitTestNNCMS
     public class AuthenticationTest
     {
         private readonly static string ApiUrl = "http://localhost:17167/";
+        private readonly static string ClientId = "AleCms";
+        private readonly static string Secret = "AleSecret";
+        private readonly static string ApiName = "CmsApi";
+        private readonly static string TestUrl = "http://localhost:17167/user";
 
         [Test]
-        public void GetAuthenticationTokenTestMethod()
+        public void AuthenticationConnectTestMethod()
         {
             var disco = DiscoveryClient.GetAsync(ApiUrl);
-            Assert.IsTrue(disco.Result.IsError);
+            Assert.IsFalse(disco.Result.IsError);
         }
+
+        [Test]
+        public void GetAuthenticationTestMethod()
+        {
+            var disco = DiscoveryClient.GetAsync(ApiUrl);
+            Assert.IsFalse(disco.Result.IsError);
+            var tokenClient = new TokenClient(disco.Result.TokenEndpoint, ClientId,Secret);
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync(ApiName);
+            Assert.IsTrue(tokenResponse.Result.IsError);
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.Result.AccessToken);
+            var response = client.GetAsync(TestUrl);
+            Assert.IsTrue(response.Result.IsSuccessStatusCode);
+        }
+
     }
 }
